f3fd071cf27878286328f7aa8fa0a56eea814821
Ofir Pardo <pardosea@gmail.com>
2019-11-04 15:41:35 +0200

    First login gossip menu

--- src/server/scripts/Custom/custom_script_loader.cpp
+++ src/server/scripts/Custom/custom_script_loader.cpp
@@ -19,6 +19,9 @@
 
 // The name of this function should match:
 // void Add${NameOfDirectory}Scripts()
-void AddCustomScripts()
-{
+
+void AddSC_FirstLogin();
+
+void AddCustomScripts() {
+    AddSC_FirstLogin();
 }
