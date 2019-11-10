a39704308eab79f6ad150f35969d610b8f1a8ef6
pardovot <pardosea@gmail.com>
2019-11-11 00:51:49 +0200

    Create and send items on login

--- src/server/scripts/Commands/cs_send.cpp
+++ src/server/scripts/Commands/cs_send.cpp
@@ -26,6 +26,8 @@
 #include "Player.h"
 #include "RBAC.h"
 #include "WorldSession.h"
+#include <iostream>
+#include "string.h"
 
 class send_commandscript : public CommandScript
 {
@@ -56,28 +58,28 @@ public:
         Player* target;
         ObjectGuid targetGuid;
         std::string targetName;
-        if (!handler->extractPlayerTarget((char*)args, &target, &targetGuid, &targetName))
+        if(!handler->extractPlayerTarget((char*)args, &target, &targetGuid, &targetName))
             return false;
 
         char* tail1 = strtok(nullptr, "");
-        if (!tail1)
+        if(!tail1)
             return false;
 
         char const* msgSubject = handler->extractQuotedArg(tail1);
-        if (!msgSubject)
+        if(!msgSubject)
             return false;
 
         char* tail2 = strtok(nullptr, "");
-        if (!tail2)
+        if(!tail2)
             return false;
 
         char const* msgText = handler->extractQuotedArg(tail2);
-        if (!msgText)
+        if(!msgText)
             return false;
 
         // msgSubject, msgText isn't NUL after prev. check
         std::string subject = msgSubject;
-        std::string text    = msgText;
+        std::string text = msgText;
 
         // from console, use non-existing sender
         MailSender sender(MAIL_NORMAL, handler->GetSession() ? handler->GetSession()->GetPlayer()->GetGUID().GetCounter() : 0, MAIL_STATIONERY_GM);
@@ -101,28 +103,28 @@ public:
         Player* receiver;
         ObjectGuid receiverGuid;
         std::string receiverName;
-        if (!handler->extractPlayerTarget((char*)args, &receiver, &receiverGuid, &receiverName))
+        if(!handler->extractPlayerTarget((char*)args, &receiver, &receiverGuid, &receiverName))
             return false;
 
-        char* tail1 = strtok(nullptr, "");
-        if (!tail1)
+        char* tail1 = strtok(NULL, "");
+        if(!tail1)
             return false;
 
         char const* msgSubject = handler->extractQuotedArg(tail1);
-        if (!msgSubject)
+        if(!msgSubject)
             return false;
 
-        char* tail2 = strtok(nullptr, "");
-        if (!tail2)
+        char* tail2 = strtok(NULL, "");
+        if(!tail2)
             return false;
 
         char const* msgText = handler->extractQuotedArg(tail2);
-        if (!msgText)
+        if(!msgText)
             return false;
 
         // msgSubject, msgText isn't NUL after prev. check
         std::string subject = msgSubject;
-        std::string text    = msgText;
+        std::string text = msgText;
 
         // extract items
         typedef std::pair<uint32, uint32> ItemPair;
@@ -133,7 +135,7 @@ public:
         char* tail = strtok(nullptr, "");
 
         // get from tail next item str
-        while (char* itemStr = strtok(tail, " "))
+        while(char* itemStr = strtok(tail, " "))
         {
             // and get new tail
             tail = strtok(nullptr, "");
@@ -143,11 +145,11 @@ public:
             char const* itemCountStr = strtok(nullptr, " ");
 
             uint32 itemId = atoi(itemIdStr);
-            if (!itemId)
+            if(!itemId)
                 return false;
 
             ItemTemplate const* item_proto = sObjectMgr->GetItemTemplate(itemId);
-            if (!item_proto)
+            if(!item_proto)
             {
                 handler->PSendSysMessage(LANG_COMMAND_ITEMIDINVALID, itemId);
                 handler->SetSentErrorMessage(true);
@@ -155,14 +157,14 @@ public:
             }
 
             uint32 itemCount = itemCountStr ? atoi(itemCountStr) : 1;
-            if (itemCount < 1 || (item_proto->MaxCount > 0 && itemCount > uint32(item_proto->MaxCount)))
+            if(itemCount < 1 || (item_proto->MaxCount > 0 && itemCount > uint32(item_proto->MaxCount)))
             {
                 handler->PSendSysMessage(LANG_COMMAND_INVALID_ITEM_COUNT, itemCount, itemId);
                 handler->SetSentErrorMessage(true);
                 return false;
             }
 
-            while (itemCount > item_proto->GetMaxStackSize())
+            while(itemCount > item_proto->GetMaxStackSize())
             {
                 items.push_back(ItemPair(itemId, item_proto->GetMaxStackSize()));
                 itemCount -= item_proto->GetMaxStackSize();
@@ -170,7 +172,7 @@ public:
 
             items.push_back(ItemPair(itemId, itemCount));
 
-            if (items.size() > MAX_MAIL_ITEMS)
+            if(items.size() > MAX_MAIL_ITEMS)
             {
                 handler->PSendSysMessage(LANG_COMMAND_MAIL_ITEMS_LIMIT, MAX_MAIL_ITEMS);
                 handler->SetSentErrorMessage(true);
@@ -186,9 +188,9 @@ public:
 
         SQLTransaction trans = CharacterDatabase.BeginTransaction();
 
-        for (ItemPairs::const_iterator itr = items.begin(); itr != items.end(); ++itr)
+        for(ItemPairs::const_iterator itr = items.begin(); itr != items.end(); ++itr)
         {
-            if (Item* item = Item::CreateItem(itr->first, itr->second, handler->GetSession() ? handler->GetSession()->GetPlayer() : 0))
+            if(Item* item = Item::CreateItem(itr->first, itr->second, handler->GetSession() ? handler->GetSession()->GetPlayer() : 0))
             {
                 item->SaveToDB(trans);              // Save to prevent being lost at next mail load. If send fails, the item will be deleted.
                 draft.AddItem(item);
@@ -210,33 +212,33 @@ public:
         Player* receiver;
         ObjectGuid receiverGuid;
         std::string receiverName;
-        if (!handler->extractPlayerTarget((char*)args, &receiver, &receiverGuid, &receiverName))
+        if(!handler->extractPlayerTarget((char*)args, &receiver, &receiverGuid, &receiverName))
             return false;
 
         char* tail1 = strtok(nullptr, "");
-        if (!tail1)
+        if(!tail1)
             return false;
 
         char* msgSubject = handler->extractQuotedArg(tail1);
-        if (!msgSubject)
+        if(!msgSubject)
             return false;
 
         char* tail2 = strtok(nullptr, "");
-        if (!tail2)
+        if(!tail2)
             return false;
 
         char* msgText = handler->extractQuotedArg(tail2);
-        if (!msgText)
+        if(!msgText)
             return false;
 
         char* moneyStr = strtok(nullptr, "");
         int32 money = moneyStr ? atoi(moneyStr) : 0;
-        if (money <= 0)
+        if(money <= 0)
             return false;
 
         // msgSubject, msgText isn't NUL after prev. check
         std::string subject = msgSubject;
-        std::string text    = msgText;
+        std::string text = msgText;
 
         // from console show nonexisting sender
         MailSender sender(MAIL_NORMAL, handler->GetSession() ? handler->GetSession()->GetPlayer()->GetGUID().GetCounter() : 0, MAIL_STATIONERY_GM);
@@ -258,15 +260,15 @@ public:
     {
         /// - Find the player
         Player* player;
-        if (!handler->extractPlayerTarget((char*)args, &player))
+        if(!handler->extractPlayerTarget((char*)args, &player))
             return false;
 
         char* msgStr = strtok(nullptr, "");
-        if (!msgStr)
+        if(!msgStr)
             return false;
 
         /// - Check if player is logging out.
-        if (player->GetSession()->isLogingOut())
+        if(player->GetSession()->isLogingOut())
         {
             handler->SendSysMessage(LANG_PLAYER_NOT_FOUND);
             handler->SetSentErrorMessage(true);
